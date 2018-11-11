a351e95e84b19d39b27419437599e682f5fb389c
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-11-11 18:00:10 +0900

    保存されている一覧の表示

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -36,4 +36,38 @@
     </div>
 
     <!-- TODO: 現在のタスク -->
+    @if (count($tasks) > 0)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                現在のタスク
+            </div>
+
+            <div class="panel-body">
+                <table class="table table-striped task-table">
+
+                    <!-- テーブルヘッダ -->
+                    <thead>
+                        <th>Task</th>
+                        <th>&nbsp;</th>
+                    </thead>
+
+                    <!-- テーブル本体 -->
+                    <tbody>
+                        @foreach ($tasks as $task)
+                            <tr>
+                                <!-- タスク名 -->
+                                <td class="table-text">
+                                    <div>{{ $task->name }}</div>
+                                </td>
+
+                                <td>
+                                    <!-- TODO: 削除ボタン -->
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
 @endsection
