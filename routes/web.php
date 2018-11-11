3e3e1112d3b738ef39304c93ddde7fa440b5b7f2
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-11-11 17:39:56 +0900

    ※※フォーム入力の正当性の確認（バリデート）
    ！ルートに記述！

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,17 @@ Route::get('/', function () {
  * 新タスク追加
  */
 Route::post('/task', function (Request $request) {
-    //
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    // タスク作成処理…
 });
 
 /**
